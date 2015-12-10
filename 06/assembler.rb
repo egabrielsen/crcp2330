d9fa600f88d5877d78ad607d46fe6692002728c1
egabrielsen <erik.gabe@gmail.com>
2015-12-09 22:39:19 -0600

    implemented some methods

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,10 +1,21 @@
 #! /usr/bin/env ruby
 
-asm_filename = ARGV[0]
+def args_valid?
+  ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1
+end
 
-if (ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1 && File.exist?(ARGV[0]) && File.readable?(ARGV[0]))
-  asm_filename = ARGV[0]
-  puts asm_filename
-else
+def is_readable?(file)
+  File.readable?(file)
+end
+
+unless args_valid?
   abort("Usage: ./assember.rb Prog.asm")
 end
+
+asm_filename = ARGV[0]
+
+unless is_readable?(asm_filename)
+  abort("#{asm_filename} is not readable or is not found")
+end
+
+puts "The contents of #{asm_filename}"
