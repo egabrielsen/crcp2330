ed5fb055bcc2949a5a8cd782bf6041f06387187e
egabrielsen <erik.gabe@gmail.com>
2015-12-10 00:30:14 -0600

    WIP on assembler

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -4,6 +4,16 @@ require_relative 'parser'
 
 class Assembler
 
+  LABELS = {
+			'SP' => 0,
+			'LCL' => 1,
+			'ARG' => 2,
+			'THIS' => 3,
+			'THAT' => 4,
+			'SCREEN' => 16384,
+			'KBD' => 24576
+		}
+
   def initialize(asm_file, hack_file)
     @asm_file = asm_file
     @hack_file = hack_file
--- 06/parser.rb
+++ 06/parser.rb
@@ -1,10 +1,54 @@
+require_relative 'code'
+
 class Parser
   def initialize(assembly_instructions)
     @assembly_instructions = assembly_instructions
+    @machine_instructions = []
   end
 
   def parse!
-    @assembly_instructions
+    @assembly_instructions.each do |instruction|
+      if command_type(instruction) == :a_command
+        @machine_instructions << assemble_a_command(instruction)
+      elsif command_type(instruction) == :c_command
+        @machine_instructions << assemble_c_command(instruction)
+      end
+    end
+    @machine_instructions
   end
 
+  def assemble_a_command(instruction)
+    command = "0"
+    command << constant(instruction[1..-1])
+  end
+
+  def assemble_c_command(instruction)
+    command = "111"
+		if instruction.include? '='
+			@instructionSegments = instruction.split('=')
+			command << @coder.comp(@instructionSegments[1])
+			command << @coder.dest(@instructionSegments[0])
+			command << '000'
+		else instruction.include? ';'
+			@instructionSegments = instruction.split(';')
+			command << @coder.comp(@instructionSegments[0])
+			command << '000'
+			command << @coder.jump(@instructionSegments[1])
+		end
+  end
+
+  def constant(value)
+    "%015b" % value
+  end
+
+  def assemble_c_command(instruction)
+    command = "111"
+  end
+
+  def command_type(instruction)
+    if instruction.start_with?('@')
+			return :a_command
+		else
+			return :c_command
+		end
 end
