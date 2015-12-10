1cc06bb9fb21ab4722e8daa515678b04246e473d
egabrielsen <erik.gabe@gmail.com>
2015-12-09 22:27:33 -0600

    file checking

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,3 +1,10 @@
 #! /usr/bin/env ruby
 
-puts "Hello Ruby!"
+asm_filename = ARGV[0]
+
+if (ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1 && File.exist?(ARGV[0]) && File.readable?(ARGV[0]))
+  asm_filename = ARGV[0]
+  puts asm_filename
+else
+  abort("Usage: ./assember.rb Prog.asm")
+end
